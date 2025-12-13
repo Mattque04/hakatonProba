096d6aba8666fd1f0660d0fb62fe4a231977751f
smatic <smatic@propeler.io>
2025-12-13 11:40:57 +0100

    structure

--- src/main/kotlin/com/github/mattque04/hakatonproba/gpt/openai_plugin_test.kt
+++ src/main/kotlin/com/github/mattque04/hakatonproba/gpt/openai_plugin_test.kt
@@ -1,4 +1,4 @@
-package com.github.mattque04.hakatonproba
+package com.github.mattque04.hakatonproba.gpt
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -7,20 +7,18 @@ import com.openai.client.okhttp.OpenAIOkHttpClient
 import com.openai.models.ChatModel
 import com.openai.models.responses.ResponseCreateParams
 
-
-
 class openai_plugin_test : AnAction("Show Action") {
 
-    var client = OpenAIOkHttpClient.builder()
-        .apiKey("pusite kurac nema api key za dzaba")
+    var client = OpenAIOkHttpClient.Companion.builder()
+        .apiKey("")
         .build();
 
 
     override fun actionPerformed(e: AnActionEvent) {
 
-        val params = ResponseCreateParams.builder()
+        val params = ResponseCreateParams.Companion.builder()
             .input("Say this is a test")
-            .model(ChatModel.GPT_4_1)
+            .model(ChatModel.Companion.GPT_4_1)
             .build()
 
         val response = client.responses().create(params);
