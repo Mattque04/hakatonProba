156f13242872e61e2ef14742ba4fd7c81ba26614
dimitrijeciric-sh <dimitrije.ciric@spothopperapp.com>
2025-12-13 15:33:53 +0100

    add summary generator

---
+++ src/main/kotlin/com/github/mattque04/hakatonproba/openai/OpenAi.kt
@@ -0,0 +1,26 @@
+package com.github.mattque04.hakatonproba.openai
+
+import com.openai.client.okhttp.OpenAIOkHttpClient
+import com.openai.models.ChatModel
+import com.openai.models.responses.ResponseCreateParams
+
+class OpenAi {
+
+    var client = OpenAIOkHttpClient.Companion.builder()
+        .apiKey("")
+        .build();
+
+    fun call(instructions: String, input: String, model: ChatModel, temperature: Double = 1.0): String {
+        val params = ResponseCreateParams.Companion.builder()
+            .instructions(instructions)
+            .input(input)
+            .model(model)
+            .temperature(temperature)
+            .build()
+
+        val response = client.responses().create(params);
+
+        return response.output().get(0).message().get().content().get(0).outputText().get().text();
+    }
+
+}
---
+++ src/main/kotlin/com/github/mattque04/hakatonproba/summary_generator/SummaryGenerator.kt
@@ -0,0 +1,13 @@
+package com.github.mattque04.hakatonproba.summary_generator
+
+class SummaryGenerator {
+
+    fun generate(filePath: String, className: String, functionName: String): String {
+
+
+
+        return "";
+    }
+
+
+}
